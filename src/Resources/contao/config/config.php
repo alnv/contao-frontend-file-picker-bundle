9c7981861f989d0ea287999c033113ef3fe3fc99
Alexander Naumov <mail@alexandernaumov.de>
2022-03-03 10:27:31 +0100

    add file type

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -4,5 +4,7 @@ $GLOBALS['TL_HOOKS']['catalogManagerRenderCatalog'][] = ['Alnv\ContaoFrontendFil
 $GLOBALS['TL_HOOKS']['catalogManagerFrontendEditingOnSave'][] = ['Alnv\ContaoFrontendFilePickerBundle\Hooks\Fields', 'prepareData'];
 $GLOBALS['TL_HOOKS']['catalogManagerInitializeFrontendEditing'][] = ['Alnv\ContaoFrontendFilePickerBundle\Hooks\Fields', 'catalogManagerInitializeFrontendEditing'];
 
+$GLOBALS['TL_CATALOG_MANAGER']['FIELD_TYPE_CONVERTER']['feFilePicker'] = 'fileTree';
+
 $GLOBALS['TL_CATALOG_MANAGER']['FIELD_TYPES']['feFilePicker'] = ['dcType' => 'dcPaletteField'];
 $GLOBALS['TL_FFL']['filePicker'] = 'Alnv\ContaoFrontendFilePickerBundle\Forms\FormFilePicker';
