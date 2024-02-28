acefc3aa201e3befc295e81bfaee645c82bffbd8
Alexander Naumov <alexandernaumov@AlexandGeschaft.fritz.box>
2024-02-28 15:26:59 +0100

    fx insert tag problem

--- src/Resources/contao/languages/en/default.php
+++ src/Resources/contao/languages/en/default.php
@@ -1,22 +1,19 @@
 <?php
 
-$GLOBALS['TL_LANG']['MSC']['filePickerCloseModal']   = 'Close';
+$GLOBALS['TL_LANG']['MSC']['filePickerCloseModal'] = 'Close';
 $GLOBALS['TL_LANG']['MSC']['filePickerSelectButton'] = 'Change selection';
-$GLOBALS['TL_LANG']['MSC']['filePickerDeleteFile']   = 'Delete file';
-$GLOBALS['TL_LANG']['MSC']['filePickerRemoveFile']   = 'Remove selection';
-$GLOBALS['TL_LANG']['MSC']['filePickerInputLabel']   = 'Select';
+$GLOBALS['TL_LANG']['MSC']['filePickerDeleteFile'] = 'Delete file';
+$GLOBALS['TL_LANG']['MSC']['filePickerRemoveFile'] = 'Remove selection';
+$GLOBALS['TL_LANG']['MSC']['filePickerInputLabel'] = 'Select';
 
-$GLOBALS['TL_LANG']['MSC']['dropzoneDzMessage']            = 'Place file here for upload.';
-$GLOBALS['TL_LANG']['MSC']['dictDefaultMessage']           = 'Place files for upload here.';
-$GLOBALS['TL_LANG']['MSC']['dictFallbackMessage']          =
-    'Your browser does not support drag \'n\' drop file uploads.';
-$GLOBALS['TL_LANG']['MSC']['dictFallbackText']             =
-    'Please use the fallback form below to upload your files like in the old days.';
-$GLOBALS['TL_LANG']['MSC']['dictFileTooBig']               =
-    'File is too large ({{filesize}}MB). Maximum file size: {{maxFilesize}}MB.';
-$GLOBALS['TL_LANG']['MSC']['dictInvalidFileType']          = 'You cannot upload files of this type - remove file.';
-$GLOBALS['TL_LANG']['MSC']['dictResponseError']            = 'The server responded with the code {{statusCode}}.';
-$GLOBALS['TL_LANG']['MSC']['dictCancelUpload']             = 'Cancel upload';
+$GLOBALS['TL_LANG']['MSC']['dropzoneDzMessage'] = 'Place file here for upload.';
+$GLOBALS['TL_LANG']['MSC']['dictDefaultMessage'] = 'Place files for upload here.';
+$GLOBALS['TL_LANG']['MSC']['dictFallbackMessage'] = 'Your browser does not support drag \'n\' drop file uploads.';
+$GLOBALS['TL_LANG']['MSC']['dictFallbackText'] = 'Please use the fallback form below to upload your files like in the old days.';
+$GLOBALS['TL_LANG']['MSC']['dictFileTooBig'] = 'File is too large ({{ filesize }}MB). Maximum file size: {{ maxFilesize }}MB.';
+$GLOBALS['TL_LANG']['MSC']['dictInvalidFileType'] = 'You cannot upload files of this type - remove file.';
+$GLOBALS['TL_LANG']['MSC']['dictResponseError'] = 'The server responded with the code {{ statusCode }}.';
+$GLOBALS['TL_LANG']['MSC']['dictCancelUpload'] = 'Cancel upload';
 $GLOBALS['TL_LANG']['MSC']['dictCancelUploadConfirmation'] = 'Are you sure you want to cancel this upload?';
-$GLOBALS['TL_LANG']['MSC']['dictRemoveFile']               = 'Remove file';
-$GLOBALS['TL_LANG']['MSC']['dictMaxFilesExceeded']         = 'You cannot upload any more files.';
+$GLOBALS['TL_LANG']['MSC']['dictRemoveFile'] = 'Remove file';
+$GLOBALS['TL_LANG']['MSC']['dictMaxFilesExceeded'] = 'You cannot upload any more files.';
